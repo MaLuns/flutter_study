cab4a409fb6fda0f5d0baa4a3174d1992918b9a0
ml <malun@cloudyigou.com>
2020-12-23 16:41:16 +0800

    upd: update

--- lib/views/sliver_demo.dart
+++ lib/views/sliver_demo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_study/components/tab_controller.dart';
 import 'package:flutter_study/models/tab_model.dart';
 
@@ -10,7 +11,7 @@ class SliverDemo extends StatelessWidget {
       title: "Sliver",
       tabModels: <TabModel>[
         TabModel(tab: Tab(text: 'SliverGrid'), page: SliverGridDemo()),
-        TabModel(tab: Tab(text: '示例二'), page: StllDemo()),
+        TabModel(tab: Tab(text: '示例二'), page: SliverGridDemoTwo()),
         TabModel(tab: Tab(text: '示例三'), page: Text('示例三')),
       ],
     );
@@ -43,28 +44,40 @@ class SliverGridDemo extends StatelessWidget {
   }
 }
 
-class StllDemo extends StatelessWidget {
-/*   Widget _viewportBuilder(BuildContext context, position) {
-    return ListView();
-  } */
-
+// 示例二
+class SliverGridDemoTwo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return CupertinoScrollbar(
-      child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        padding: EdgeInsets.all(16.0),
-        child: Center(
-          child: Column(
-            //动态创建一个List<Widget>
-            children: str
-                .split("")
-                .map((c) => Text(c, textScaleFactor: 2.0))
-                .toList(),
+    return CustomScrollView(
+      physics: BouncingScrollPhysics(),
+      slivers: [
+        SliverPadding(
+          padding: EdgeInsets.all(8),
+          sliver: SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (contet, index) => Container(color: Colors.red[200]),
+              childCount: 6,
+            ),
           ),
         ),
-      ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return Container(
+                height: 80,
+                color: Colors.black12,
+                margin: EdgeInsets.all(8),
+              );
+            },
+            childCount: 5,
+          ),
+        ),
+      ],
     );
   }
 }
